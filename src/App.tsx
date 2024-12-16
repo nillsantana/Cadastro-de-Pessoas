67755abf49835a59628065f0c4bc2be0923f0e2b
Nill Santana <nillsantana25@gmail.com>
2024-12-16 14:52:12 -0300

    add theme MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter } from "react-router-dom"
 import { AppRoutes } from "./routes"
+import { ThemeProvider } from "@emotion/react"
+import { light } from "@mui/material/styles/createPalette"
+import { LightTheme } from "./shared/themes"
 
 export const App = () => {
   return (
-    
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
